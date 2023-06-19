a0d3508991734c0b10b827015d04f0deea931551
DirtdiverIV <davidgomez.seg@gmail.com>
2023-06-19 12:45:08 +0200

    Finished Exercise

---
+++ myscripts.js
@@ -0,0 +1,15 @@
+function fadeSqu() {
+    document.getElementById("squ").style.backgroundColor = "transparent";
+  }
+function fadeTri() {
+    document.getElementById("tri").style.borderColor = "transparent";
+  }
+function fadeCir() {
+    document.getElementById("cir").style.backgroundColor = "transparent";
+  }
+
+  function reset() {
+    document.getElementById("cir").style.backgroundColor = "blue";
+    document.getElementById("tri").style.borderBottomColor = "yellow";
+    document.getElementById("squ").style.backgroundColor = "red";
+  }
